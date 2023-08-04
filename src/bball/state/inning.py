7f34a753cacf8c70fc243060b4911f7f28820041
dpasse <50343386+dpasse@users.noreply.github.com>
2023-08-04 08:55:23 -0500

    clean up

--- src/bball/state/inning.py
+++ src/bball/state/inning.py
@@ -1,20 +1,18 @@
-from typing import List, Optional
+from typing import Optional
 
 from .bases import Bases
 from .calculators.outs import OutsCalculator
-from ..models import EventCodes, InningContext, InningHistory
+from ..models import EventCodes, InningContext
 
 
 class Inning():
-    def __init__(self, state: Optional[InningContext] = None) -> None:
-        self._bases = Bases(state.bases if state else [0, 0, 0])
-        self._runs: int = state.runs if state else 0
-        self._outs: int = state.outs if state else 0
+    def __init__(self) -> None:
+        self._bases = Bases([0, 0, 0])
+        self._runs: int = 0
+        self._outs: int = 0
+
         self._outs_calculator = OutsCalculator()
 
-    def is_over(self) -> bool:
-        return self._outs >= 3
-    
     @property
     def current_state(self) -> InningContext:
         return InningContext(
@@ -23,7 +21,16 @@ class Inning():
             self._runs
         )
 
+    def is_over(self) -> bool:
+        return self._outs >= 3
+
     def execute(self, event_code: EventCodes) -> None:
         self._outs += self._outs_calculator.calculate(event_code)
         if not self.is_over():
             self._runs += self._bases.play_event(event_code)
+
+    def set_state(self, state: InningContext) -> None:
+        self._bases = Bases(state.bases)
+        self._runs: int = state.runs
+        self._outs: int = state.outs
+    
